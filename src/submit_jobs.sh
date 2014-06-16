b724f36c476f8b3246c3e645cb6811daf9475ee7
Rohit Girdhar <rohit@iiith>
2014-06-17 02:27:34 +0530

    minor bug fix

--- src/submit_jobs.sh
+++ src/submit_jobs.sh
@@ -3,5 +3,5 @@ NUM_IMGS=5062
 rm -r std/err std/out
 mkdir -p std/out std/err
 for (( i = 1; i <= ${NUM_IMGS}; i++ )) {
-	qsub -cwd -o std/out/$i.out -e std/err/$i.out /run.sh ./matlab_batcher.sh runGISTCompute $i,${NUM_IMGS}
+	qsub -cwd -o std/out/$i.out -e std/err/$i.err ./run.sh ./matlab_batcher.sh runGISTCompute $i,${NUM_IMGS}
 }
